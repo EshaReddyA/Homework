a1603dd3e7d10074e8413c1a78dd4526ca0a1d54
Esha Reddy Annapureddy <eshasplej5@gmail.com>
2025-03-23 18:59:11 -0500

    calculator

---
+++ Calculator.py
@@ -0,0 +1,14 @@
+x = 5
+y = 6
+#addition
+Add = x + y
+print(f"add:{Add}")
+#subtraction
+Sub = x - y
+print(f"Sub:{Sub}")
+#Multiplication
+mul = x * y
+print(f"Mul:{mul}")
+#Division
+div = x / y
+print(f"Div:{div}")
